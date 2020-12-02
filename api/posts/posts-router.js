1ee6428a094b7954cdbae3b24debd4e7e98875c8
tarmes <traarmes@gmail.com>
2020-12-02 18:49:51 -0500

    get request working correctly

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,5 +1,28 @@
+const { Router } = require('express')
 const express = require('express')
 
 const router = express.Router()
+
+const postsDB = require('../../data/db')
+
+// POSTS ENDPOINTS
+
+// [GET]
+
+router.get('/', async (req, res) => {
+   const { query } = req
+   try {
+      const posts = await postsDB.find(query)
+      res.json(posts)
+   } catch (error) {
+      res.json(error.message)
+   }
+})
+
+// [POST]
+
+// [DELETE]
+
+// [PUT]
 
 module.exports = router
--- api/server.js
+++ api/server.js
@@ -2,11 +2,11 @@ const express = require('express')
 const cors = require('cors')
 const server = express()
 
-const router = require('./posts/posts-router')
+const postsRouter = require('./posts/posts-router')
 
 server.use(cors())
 server.use(express.json())
-server.use('/api/posts', router)
+server.use('/api/posts', postsRouter)
 
 server.get('/', (req, res) => {
    res.send(`
