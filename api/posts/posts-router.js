4579c0ee1cd229c00c11781e579294e95a2f57c6
tarmes <traarmes@gmail.com>
2020-12-02 19:34:18 -0500

    get request by id

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,13 +1,12 @@
-const { Router } = require('express')
 const express = require('express')
+const postsDB = require('../../data/db')
 
 const router = express.Router()
 
-const postsDB = require('../../data/db')
 
 // POSTS ENDPOINTS
 
-// [GET]
+// [GET] all posts
 
 router.get('/', async (req, res) => {
    const { query } = req
@@ -15,9 +14,40 @@ router.get('/', async (req, res) => {
       const posts = await postsDB.find(query)
       res.json(posts)
    } catch (error) {
-      res.json(error.message)
+      res.status(500).json({ error: "The posts information could not be retrieved."})
    }
 })
+
+// [GET] post by :id
+
+router.get('/:id', async (req, res) => {
+   const { id } = req.params
+   try {
+      const post = await postsDB.findById(id)
+      if (!post) {
+         res.status(404).json({ message: "The post with the specified ID does not exist." })
+      } else {
+         res.status(200).json(post)
+      }
+   } catch (error) {
+      res.status(500).json(error.message)
+   }
+})
+
+// router.get('/:id', (req, res) => {
+//    const id = req.params.id
+//    postsDB.findById(id)
+//    .then(post => {
+//       if (post) {
+//          res.status(200).json(post)
+//       } else {
+//          res.status(404).json({ message: "The post with the specified ID does not exist." })
+//       }
+//    })
+//    .catch(error => {
+//       res.status(500).json({ error: "The post information could not be retrieved." })
+//    })
+// })
 
 // [POST]
 
